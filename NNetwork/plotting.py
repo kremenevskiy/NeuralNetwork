3169e4c20d827ef9148c4e64853d2753c9d1cd3e
kremenevskiy <kremeneuski@gmail.com>
2021-11-24 02:32:48 +0300

    add: overlay accuracy plot

--- NNetwork/plotting.py
+++ NNetwork/plotting.py
@@ -39,7 +39,7 @@ def plot_training_accuracy(training_accuracy, num_epochs, training_accuracy_xmin
     ax.grid(True)
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Accuracy on train')
-    ax.set_title('Accuracy (%) on the training data')
+    ax.set_title('Accuracy on the training data')
     plt.show()
 
 
@@ -52,7 +52,22 @@ def plot_validation_accuracy(validation_accuracy, num_epochs, test_accuracy_xmin
     ax.grid(True)
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Accuracy on validation')
-    ax.set_title('Accuracy (%) on the validation data')
+    ax.set_title('Accuracy on the validation data')
+    plt.show()
+
+
+def plot_overlay(validation_accuracy, training_accuracy, num_epochs, xmin=0):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(xmin, num_epochs), training_accuracy, color='orange', label='Accuracy on the train data')
+    ax.plot(np.arange(xmin, num_epochs), validation_accuracy, color='blue', label='Accuracy on the validation data')
+    ax.grid(True)
+    ax.set_xlim([xmin, num_epochs])
+    ax.set_ylim([0.9, 1])
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Compare accuracies on test and validation')
+    plt.legend(loc="lower right")
     plt.show()
 
 
@@ -65,3 +80,4 @@ def make_plots(data, num_epochs, training_cost_xmin=0,
     plot_training_accuracy(train_acc, num_epochs, train_accuracy_xmin)
     plot_val_cost(val_cost, num_epochs, validation_cost_xmin)
     plot_validation_accuracy(val_acc, num_epochs, validation_accuracy_xmin)
+    plot_overlay(val_acc, train_acc, num_epochs)
