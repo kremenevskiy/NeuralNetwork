2343e0ecf1393370bab211e799c70f81575288ab
kremenevskiy <kremeneuski@gmail.com>
2021-11-24 16:18:04 +0300

    upd: move data init in mnist loader file

--- NNetwork/mnist_loader.py
+++ NNetwork/mnist_loader.py
@@ -1,2 +1,23 @@
+import numpy as np
 from keras.datasets import mnist
 data_mnist = mnist.load_data()
+
+
+def vectorized_result(j):
+    res = np.zeros((10, 1))
+    res[j] = 1
+    return res
+
+
+train, test = data_mnist
+
+x_train = [np.reshape(x / 255, (784, 1)) for x in train[0]]
+y_train = [vectorized_result(j) for j in train[1]]
+x_test = [np.reshape(x / 255, (784, 1)) for x in test[0]]
+y_test = [j for j in test[1]]
+
+training_data = list(zip(x_train, y_train))
+test_data = list(zip(x_test, y_test))
+
+mnist_loader = (training_data, test_data)
+
