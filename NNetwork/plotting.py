d6af4200ec3361e842bb8441ff19fe3f8dd9faa9
kremenevskiy <kremeneuski@gmail.com>
2021-11-24 19:30:03 +0300

    add: overlay plot to compare validation and test accuracy

--- NNetwork/plotting.py
+++ NNetwork/plotting.py
@@ -56,17 +56,17 @@ def plot_validation_accuracy(validation_accuracy, num_epochs, test_accuracy_xmin
     plt.show()
 
 
-def plot_overlay(validation_accuracy, training_accuracy, num_epochs, xmin=0):
+def plot_overlay(validation_accuracy, training_accuracy, num_epochs, ylim=0.9, xmin=0):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(np.arange(xmin, num_epochs), training_accuracy, color='orange', label='Accuracy on the train data')
     ax.plot(np.arange(xmin, num_epochs), validation_accuracy, color='blue', label='Accuracy on the validation data')
     ax.grid(True)
     ax.set_xlim([xmin, num_epochs])
-    ax.set_ylim([0.9, 1])
+    ax.set_ylim([ylim, 1])
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Accuracy')
-    ax.set_title('Compare accuracies on test and validation')
+    ax.set_title('Compare accuracy on test and validation')
     plt.legend(loc="lower right")
     plt.show()
 
@@ -74,10 +74,11 @@ def plot_overlay(validation_accuracy, training_accuracy, num_epochs, xmin=0):
 def make_plots(data, num_epochs, training_cost_xmin=0,
                train_accuracy_xmin=0,
                validation_cost_xmin=0,
-               validation_accuracy_xmin=0):
+               validation_accuracy_xmin=0,
+               overlay_ylim=0.9):
     val_cost, val_acc, train_cost, train_acc = data[0], data[1], data[2], data[3]
     plot_training_cost(train_cost, num_epochs, training_cost_xmin)
     plot_training_accuracy(train_acc, num_epochs, train_accuracy_xmin)
     plot_val_cost(val_cost, num_epochs, validation_cost_xmin)
     plot_validation_accuracy(val_acc, num_epochs, validation_accuracy_xmin)
-    plot_overlay(val_acc, train_acc, num_epochs)
+    plot_overlay(val_acc, train_acc, num_epochs, ylim=overlay_ylim)
