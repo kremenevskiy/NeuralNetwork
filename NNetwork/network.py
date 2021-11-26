c3a32410bf17f9ec6c822d29a1ae6d71dd1371ce
kremenevskiy <kremeneuski@gmail.com>
2021-11-26 16:38:05 +0300

    fix: saving biases to file

--- NNetwork/network.py
+++ NNetwork/network.py
@@ -57,7 +57,6 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
         self.weights = [np.random.randn(x, y) for x, y in zip(self.sizes[1:], self.sizes[:-1])]
 
-
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a) + b)
@@ -165,7 +164,7 @@ class Network(object):
         data = {
             "sizes": list(self.sizes),
             "weights": [w.tolist() for w in self.weights],
-            "biases": [b.tolist() for b in self.weights],
+            "biases": [b.tolist() for b in self.biases],
             "cost": str(self.cost.__name__)
         }
         with open(filename, 'w') as f:
